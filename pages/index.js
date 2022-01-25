cb825039381b4d18c45fc546a22c4022628a5a24
Victor <victor_martins_castro@hotmail.com>
2022-01-25 12:13:48 -0300

    User Change and New Page

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
 import appConfig from "../config.json";
+import { useRouter } from 'next/router';
 
-
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
 
 function Title(props) {
     const Tag = props.tag || 'h1';
@@ -64,11 +37,12 @@ function Title(props) {
 //   export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'victormcfreire';
+  //const username = 'victormcfreire';
+  const [username, setUsername] = React.useState('victormcfreire');
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -95,6 +69,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={ function (eventInfo) {
+              eventInfo.preventDefault();
+              roteamento.push('/chat');
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -114,6 +92,12 @@ export default function PaginaInicial() {
                   mainColorHighlight: appConfig.theme.colors.primary[500],
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
+              }}
+
+              value = {username}
+              onChange={ function Handler(event) {
+                const valor = event.target.value;
+                setUsername(valor);
               }}
             />
             <Button
